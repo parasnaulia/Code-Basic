a11cc57670b43ac87805502914489d4c00c76d90
Paras Naulia <parasnaulia645@gmail.com>
2024-08-05 20:42:15 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Nav from './Nav'
 import './App.css'
+import Todo from './Todo'
+import { Provider, useSelector } from 'react-redux'
+import { store } from './assets/Store/Store'
+import MainTodo from './MainTodo'
+import Calendar from 'react-calendar'
+import Friend from './Friend'
+import { IoMdAddCircle } from "react-icons/io";
+import Empty from './Empty'
+import { Outlet } from 'react-router-dom'
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-  const increment=()=>{
-    setCount(count+1);
-  }
 
   return (
-    <>
-      <div>
-        <button onClick={increment}>Count This is Done By Sub This is me</button>
-       {count}
-     </div>
-    </>
+   
+    <div className='Container'>
+      <Nav/>
+     
+        
+     <Outlet/>
+     
+
+      
+   
+   
+    </div>
+     
+ 
+   
+ 
   )
 }
 
